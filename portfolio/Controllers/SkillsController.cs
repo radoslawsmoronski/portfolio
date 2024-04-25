60bc853e5f98474219e582c0028c99941fb54bee
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-04-25 17:11:20 +0200

    I added edit and delete functionality

--- portfolio/Controllers/SkillsController.cs
+++ portfolio/Controllers/SkillsController.cs
@@ -23,5 +23,72 @@ namespace portfolio.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Skill obj)
+        {
+            if(ModelState.IsValid)
+            {
+                obj.ImageUrl = "images/csharp.png";
+                _db.Skills.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            Skill? skillFromDb = _db.Skills.Find(id);
+
+            if(skillFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(skillFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Skill obj)
+        {
+            if (ModelState.IsValid)
+            {
+                obj.ImageUrl = "images/csharp.png";
+                _db.Skills.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpPost, ActionName("delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Skill? skillFromDb = _db.Skills.Find(id);
+
+            if (skillFromDb == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Skills.Remove(skillFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
--- portfolio/Models/Skill.cs
+++ portfolio/Models/Skill.cs
@@ -7,6 +7,7 @@ namespace portfolio.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(25)]
         public string Name { get; set; }
         [Required]
         public string ImageUrl { get; set; }
