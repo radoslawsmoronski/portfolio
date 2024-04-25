bcab428fd80ae62f0960a730e51c582e6bef6108
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-04-25 14:25:50 +0200

    I finished skills view

---
+++ portfolio/Controllers/SkillsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio.Controllers
+{
+    public class SkillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
