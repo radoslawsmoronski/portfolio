f5589e39ce1977d5d1557d19c3f63dfdfb935f54
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-05-07 13:56:05 +0200

    Added Contact class

---
+++ portfolio.Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public string Icon { get; set; }
+    }
+}
