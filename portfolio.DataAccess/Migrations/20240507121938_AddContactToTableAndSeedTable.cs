39e6aaedae3d741348537f12cd3f4f8c7151f81c
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-05-07 14:21:04 +0200

    Addded migration to db for Contact model

---
+++ portfolio.DataAccess/Migrations/20240507121938_AddContactToTableAndSeedTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace portfolio.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContactToTableAndSeedTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Contacts",
+                columns: new[] { "Id", "Content", "Icon", "Name" },
+                values: new object[,]
+                {
+                    { 1, "email@sample.com", "bi bi-envelope", "Sample" },
+                    { 2, "email_2@sample.com", "bi bi-envelope", "Sample 2" },
+                    { 3, "email_3@sample.com", "bi bi-envelope", "Sample 3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
