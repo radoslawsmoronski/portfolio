da93e6e46de8cc7e7d64e69cfa5ff6d8769af293
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-05-08 17:01:45 +0200

    I modified ContactForm class

--- portfolio.Models/ContactForm.cs
+++ portfolio.Models/ContactForm.cs
@@ -12,6 +12,10 @@ namespace portfolio.Models
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Subject { get; set; }
 
         [Required]
         public string Content { get; set; }
