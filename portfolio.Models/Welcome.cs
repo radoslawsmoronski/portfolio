1dff2e26227fe63793b96792f1149fe09b1369ee
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-05-10 18:41:00 +0200

    Implemented Welcome model to View/Home/Index and i did backend staff for this

--- portfolio.Models/Welcome.cs
+++ portfolio.Models/Welcome.cs
@@ -8,7 +8,7 @@ namespace portfolio.Models
 {
     public class Welcome
     {
-        public string Header { get; set; }
-        public string Description { get; set; }
+        public string Header { get; set; } = "Name and Surname";
+        public string Description { get; set; } = "Short description";
     }
 }
