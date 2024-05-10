958ff3c71985ed7a04def55b6a587e3ab75813a8
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-05-10 18:35:29 +0200

    I added Welcome class

---
+++ portfolio.Models/Welcome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio.Models
+{
+    public class Welcome
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+    }
+}
