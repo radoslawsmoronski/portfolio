349135236145d8c8de34af333126cd5cdcac2c02
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-05-13 15:44:39 +0200

    I created JsonClientGetter and implement it into _Layout.cstml

---
+++ portfolio.DataAccess/Json/JsonClientGetter.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio.DataAccess.Json
+{
+    public static class JsonClientGetter<T> where T : class
+    {
+        public static T Get()
+        {
+            if (typeof(T) == typeof(NavbarLogo))
+            {
+                return JsonFileManager<T>.Get();
+            }
+            else
+            {
+                throw new InvalidOperationException("Unsupported type.");
+            }
+        }
+    }
+}
