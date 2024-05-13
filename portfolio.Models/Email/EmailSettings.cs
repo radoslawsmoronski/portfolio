706b593f2e35d6a3cda57180ff55e7447fc5587b
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-05-13 14:48:55 +0200

    I added PasswordManager class and removed sending password to details and editEmail .cshtmls

--- portfolio.Models/Email/EmailSettings.cs
+++ portfolio.Models/Email/EmailSettings.cs
@@ -6,7 +6,7 @@ namespace portfolio.Models.Email
     public class EmailSettings
     {
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public string SmtpServer { get; set; }
         public int SmtpPort { get; set;}
         public bool Encryption  { get; set; }
