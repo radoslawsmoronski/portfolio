c4d7538fc7173ce2cddbbd3287d3fa0aba05ebd9
Radoslaw Smoronski <radoslaw.smoronski@gmail.com>
2024-05-15 18:23:10 +0200

    I Added Contact settings index and created basic controller to view index.cshtml

---
+++ portfolioASP/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using portfolio.DataAccess.Repository.IRepository;
+using portfolio.Models.Email;
+using portfolio.Models.ViewModels;
+
+namespace portfolioASP.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ContactController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly EmailSettings _emailSettings;
+
+        public ContactController(IUnitOfWork unitOfWork, IOptions<EmailSettings> emailSettings)
+        {
+            _unitOfWork = unitOfWork;
+            _emailSettings = emailSettings.Value;
+        }
+
+        public IActionResult Index()
+        {
+            List<EmailMessage>? emailMessages = _unitOfWork.EmailMessageRepository.GetAll().ToList();
+
+            if (emailMessages == null) return NotFound();
+
+            emailMessages.Reverse();
+
+            var viewModel = new AdminEmailsViewModel
+            {
+                EmailMessages = emailMessages,
+                UnreadEmailMessages = _unitOfWork.EmailMessageRepository.GetUnreadAmount()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
